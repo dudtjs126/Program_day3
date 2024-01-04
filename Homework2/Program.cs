9c9be8d2358679c7591a4d13e8511f1301a8e33a
dudtjs126 <dudtjs126@gmail.com>
2024-01-04 20:50:51 +0900

    띄어쓰기 수정

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Homework2
+﻿using System.Runtime.InteropServices;
+
+namespace Homework2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // 숙제 1번
-            /*{
+            {
                 Random random = new Random();
                 int number = random.Next(1, 101);
                 int count = 0;
@@ -14,25 +16,35 @@
 
                 while (true)
                 {
+                    
                     Console.Write("숫자를 입력하세요: ");
-                    int playerNumber = int.Parse(Console.ReadLine());
+                    string playerInput = Console.ReadLine();
+                    int playerNumber;
+                    bool isSuccess = int.TryParse(playerInput, out playerNumber);
                     count++;
 
-                    if (playerNumber < number)
+                    if (isSuccess)
                     {
-                        Console.WriteLine("너무 작습니다!");
-                    }
-                    else if (playerNumber > number)
-                    {
-                        Console.WriteLine("너무 큽니다!");
+                        if (playerNumber < number)
+                        {
+                            Console.WriteLine("너무 작습니다!");
+                        }
+                        else if (playerNumber > number)
+                        {
+                            Console.WriteLine("너무 큽니다!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("축하합니다!  {0}번 만에 숫자를 맞추었습니다.", count);
+                            break;
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("축하합니다!  {0}번 만에 숫자를 맞추었습니다.", count);
-                        break;
+                        Console.Write("숫자가 아닙니다. 숫자를 입력해주세요.\n");
                     }
                 }
-            }*/
+            }
 
 
 
@@ -41,34 +53,34 @@
                 Console.WriteLine("플레이어 1: X 와 플레이어 2: O\n");
                 Console.WriteLine("플레이어 1의 차례\n");
 
-                int[,] arrange = new int[9, 17] 
+                int[,] arrange = new int[9, 17]
                 {
                     {0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0},
-                    { 0, 0, 1, 0, 0, 10, 0, 0, 2, 0, 0, 10, 0, 0, 3, 0, 0},
-                    { 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11},
-                    { 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0},
-                    { 0, 0, 4, 0, 0, 10, 0, 0, 5, 0, 0, 10, 0, 0, 6, 0, 0},
-                    { 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11},
-                    { 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0},
-                    { 0, 0, 7, 0, 0, 10, 0, 0, 8, 0, 0, 10, 0, 0, 9, 0, 0},
-                    { 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0}
+                    {0, 0, 1, 0, 0, 10, 0, 0, 2, 0, 0, 10, 0, 0, 3, 0, 0},
+                    {11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11},
+                    {0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0},
+                    {0, 0, 4, 0, 0, 10, 0, 0, 5, 0, 0, 10, 0, 0, 6, 0, 0},
+                    {11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11, 10, 11, 11, 11, 11, 11},
+                    {0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0},
+                    {0, 0, 7, 0, 0, 10, 0, 0, 8, 0, 0, 10, 0, 0, 9, 0, 0},
+                    {0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0}
                 };
 
                 for (int i = 0; i < 9; i++)
                 {
                     for (int j = 0; j < 17; j++)
                     {
-                        if (arrange[i,j] == 0)
+                        if (arrange[i, j] == 0)
                         {
                             Console.Write(" ");
                         }
-                        else if (arrange[i,j] < 10)
+                        else if (arrange[i, j] < 10)
                         {
-                            Console.Write(arrange[i,j]);
+                            Console.Write(arrange[i, j]);
                         }
                         else
                         {
-                            if (arrange[i,j] == 10)
+                            if (arrange[i, j] == 10)
                             {
                                 Console.Write("|");
                             }
---
+++ parameter/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace parameter
+{
+    internal class Program
+    {
+        static void printLine()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("=");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        static void printLine2(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("=");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // --------------------------------------------
+        static float AddNumbers(float a, float b)
+        {
+            return a + b;
+        }
+
+
+        static int AddNumbers (int a, int b)
+        {
+            return a + b;
+        }
+
+
+        static int AddNumbers (int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        //----------------------------------------------
+
+        static void Main(string[] args)
+        {
+            printLine();
+            printLine2(20);
+            int result = Add(10, 20);
+            Console.WriteLine(result);
+
+
+            //------------------------------------------
+            int sum1 = AddNumbers(10, 20);
+            float sum2 = AddNumbers(10.5f, 21.5f);
+            int sum3 = AddNumbers(10, 20, 30);
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(sum3);
+            //-------------------------------------------
+        }
+    }
+}
